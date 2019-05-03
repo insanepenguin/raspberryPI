e9e23247436ba541d5732c0b654eaadc3b907e33
Edmond <edthetiger@gmail.com>
2019-05-03 23:47:42 +0000

    PI update

--- GPIO.py
+++ GPIO.py
@@ -7,7 +7,7 @@ buttonb1 = Button(13)
 ledyellow = LED(17)
 
 while True:
-    buttont1.wait_for_press = down.dwn
+    buttont1.wait_for_press = down.dwn()
     buttont2.wait_for_press = ledyellow.on
-    buttont2.wait_for_released = ledyellow.off
-    buttonb1.wait_for_press = down.newmeth
+    buttont2.wait_for_release = ledyellow.off
+    buttonb1.wait_for_press = down.newmeth()
