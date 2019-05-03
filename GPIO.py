466e368e6bcc2d83412f5cd6147a51baad2fe7e1
Edmond Behaeghel <edthetiger@gmail.com>
2019-05-03 19:46:35 -0400

    2ed Iteration of the python code

--- GPIO.py
+++ GPIO.py
@@ -7,7 +7,11 @@ buttonb1 = Button(13)
 ledyellow = LED(17)
 
 while True:
-    buttont1.wait_for_press = down.dwn
-    buttont2.wait_for_press = ledyellow.on
-    buttont2.wait_for_released = ledyellow.off
-    buttonb1.wait_for_press = down.newmeth
+    if buttont1.is_pressed:
+        print("One")
+    elif buttont2.is_pressed:
+        print("Two")
+        down.newmeth()
+    elif buttonb1.is_pressed and buttont1.is_pressed:
+        print("Three")
+        down.dwn()
