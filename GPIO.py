1f63a6a1867177ca9355b1d9f4c5362214276a64
Edmond <edthetiger@gmail.com>
2019-05-04 02:52:00 +0000

    modified:   GPIO.py: 	modified:   __pycache__/down.cpython-34.pyc

--- GPIO.py
+++ GPIO.py
@@ -7,12 +7,6 @@ buttonb1 = Button(13)
 ledyellow = LED(17)
 
 while True:
-<<<<<<< HEAD
-    buttont1.wait_for_press = down.dwn()
-    buttont2.wait_for_press = ledyellow.on
-    buttont2.wait_for_release = ledyellow.off
-    buttonb1.wait_for_press = down.newmeth()
-=======
     if buttont1.is_pressed:
         print("One")
     elif buttont2.is_pressed:
@@ -21,4 +15,4 @@ while True:
     elif buttonb1.is_pressed and buttont1.is_pressed:
         print("Three")
         down.dwn()
->>>>>>> raspberryPI/master
+
