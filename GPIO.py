af5b8ef5f1bc2ec34d70c950d06b21492ea25a96
Edmond Behaeghel <edthetiger@gmail.com>
2019-05-04 23:03:38 -0400

    3ed Iteration of the python code Added more complex button pressing system to avoid infinite loop

--- GPIO.py
+++ GPIO.py
@@ -2,19 +2,14 @@ from gpiozero import Button , LED
 import down
 
 buttont1 = Button(23)
-buttont2 = Button(24)
-buttonb1 = Button(13)
+buttonb1 = Button(26)
 ledyellow = LED(17)
-x = False
-y = False
+ledgreen = LED(6)
 while True:
-    buttont1.when_pressed = ledyellow.on()
-    buttont1.when_released = ledyellow.off()
-    buttont2.when_pressed = ledyellow.on()
-    buttont2.when_released = ledyellow.off()
-    buttonb1.when_pressed = ledyellow.on()
-    buttonb1.when_released = ledyellow.off()
-    if(x == True and y == True):
-        down.dwn()
+    buttont1.when_pressed = down.dwn()
+    buttont1.when_released = ledyellow.on
+    buttonb1.when_pressed = down.startServer()
+    buttonb1.when_released = ledgreen.on
+
 
 
--- down.py
+++ down.py
@@ -1,6 +1,16 @@
 import subprocess
+import http.server
+import socketserver
+
+
+
 def dwn():
     print ("Shunting Down")
-    subprocess.call("reboot", shell=True)
+    subprocess.call("sudo reboot", shell=True)
 def newmeth():
     print("Hello World")
+def startServer():
+    PORT = 8080
+    Handler = http.server.TCPServer(("",PORT),Handler) as httpd:
+        print("Serving at port", PORT)
+        httpd.serve_forever()
